fcd0fddbdcabefcacdebf1a6517350a3f90d7109
sestinj <33237525+sestinj@users.noreply.github.com>
2018-11-30 17:37:51 -0500

    commit (big)

---
+++ PixelStructs.swift
@@ -0,0 +1,74 @@
+//
+//  Structs.swift
+//  Photo Edit
+//
+//  Created by Nate Sesti on 4/16/18.
+//  Copyright © 2018 Nate Sesti. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreImage
+
+struct Pixel {
+    var value: UInt32
+    var red: UInt8 {
+        get { return UInt8(value & 0xFF) }
+        set { value = UInt32(newValue) | (value & 0xFFFFFF00) }
+    }
+    var green: UInt8 {
+        get { return UInt8((value >> 8) & 0xFF) }
+        set { value = (UInt32(newValue) << 8) | (value & 0xFFFF00FF) }
+    }
+    var blue: UInt8 {
+        get { return UInt8((value >> 16) & 0xFF) }
+        set { value = (UInt32(newValue) << 16) | (value & 0xFF00FFFF) }
+    }
+    var alpha: UInt8 {
+        get { return UInt8((value >> 24) & 0xFF) }
+        set { value = (UInt32(newValue) << 24) | (value & 0x00FFFFFF) }
+    }
+}
+
+
+struct RGBA {
+    var pixels: UnsafeMutableBufferPointer<Pixel>
+    var width: Int
+    var height: Int
+    
+    init?(image: UIImage) {
+        guard let cgImage = image.cgImage else { return nil } // 1
+        
+        width = Int(image.size.width)
+        height = Int(image.size.height)
+        let bitsPerComponent = 8 // 2
+        
+        let bytesPerPixel = 4
+        let bytesPerRow = width * bytesPerPixel
+        let imageData = UnsafeMutablePointer<Pixel>.allocate(capacity: width * height)
+        let colorSpace = CGColorSpaceCreateDeviceRGB() // 3
+        
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
+        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+        guard let imageContext = CGContext(data: imageData, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else { return nil }
+        imageContext.draw(cgImage, in: CGRect(origin: CGPoint.zero, size: image.size)) // 4
+        
+        pixels = UnsafeMutableBufferPointer<Pixel>(start: imageData, count: width * height)
+    }
+    
+    func toUIImage() -> UIImage? {
+        let bitsPerComponent = 8 // 1
+        
+        let bytesPerPixel = 4
+        let bytesPerRow = width * bytesPerPixel
+        let colorSpace = CGColorSpaceCreateDeviceRGB() // 2
+        
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
+        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+        let imageContext = CGContext(data: pixels.baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil)
+        guard let cgImage = imageContext!.makeImage() else {return nil} // 3
+        
+        let image = UIImage(cgImage: cgImage)
+        return image
+    }
+}
